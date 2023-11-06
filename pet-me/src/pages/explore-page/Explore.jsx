c9f99547c48a7a92238a3fd871d2fb3cb0aed13e
osman122 <osoconan1@gmail.com.com>
2023-11-06 15:31:42 +0200

    separate react

---
+++ pet-me/src/pages/explore-page/Explore.jsx
@@ -0,0 +1,16 @@
+import ExploreCard from "../../components/ExploreCard";
+const Explore = () => {
+    return ( 
+        <div className="row">
+            <ExploreCard/>
+            <ExploreCard/>
+            <ExploreCard/>
+            <ExploreCard/>
+            <ExploreCard/>
+            <ExploreCard/>
+            
+        </div>
+     );
+}
+ 
+export default Explore;

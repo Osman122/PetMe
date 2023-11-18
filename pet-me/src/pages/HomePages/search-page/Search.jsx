b1ec798e095f318be7a6a755c4d1293b6d33e6a0
omar-syed <omarrrrr118@gmail.com>
2023-11-18 18:21:09 +0200

    add functionality of Search

--- pet-me/src/pages/HomePages/search-page/Search.jsx
+++ pet-me/src/pages/HomePages/search-page/Search.jsx
@@ -7,24 +7,70 @@ import { useEffect, useState } from "react"
 import { axiosInstance } from '../../../api/config';
 
 const Search = () => {
-    const [ page, setPage ] = useState(1)
-    const [ filter, setFilter ] = useState('offers')
-    const [ maxpages, setmaxpages ] = useState(1)
-    const [ resultList, setResultList ] = useState([])
-    let {keyword: key} = useParams()
-    const [ keyword, setkeyword ] = useState(key)
+    const [ page, setPage ] = useState(1);
+    const [ filter, setFilter ] = useState('offers');
+    const [ maxpages, setmaxpages ] = useState(1);
+    const [ resultList, setResultList ] = useState([]);
+    
+    let { keyword: key } = useParams();
+    // const [keyword, setKeyword] = useState(key);
+    const [keyword, setKeyword] = useState('');
+
+    const handleKeywordChange = (event) => {
+    setKeyword(event.target.value);
+    };
 
 
 
-    useEffect(()=>{
-        axiosInstance.get(`/${filter}/?page=${page}&search=${keyword}`)
-        .then((res)=>{
-            setResultList(res.data.results)
-            setmaxpages(res.data.total_pages)
-        }).catch((error) => {
-            console.log(error)
-    })},[page, filter])
+     // Function to handle button click for Users
+    const handleUsersClick = () => {
+        setFilter('users');
+        fetchData('users');
+    };
 
+    // Function to handle button click for Pets
+    const handlePetsClick = () => {
+        setFilter('pets');
+        fetchData('pets');
+    };
+
+    // Function to handle button click for Posts
+    const handlePostsClick = () => {
+        setFilter('posts');
+        fetchData('posts');
+    };
+
+    // Function to handle button click for Offers
+    const handleOffersClick = () => {
+        setFilter('offers');
+        fetchData('offers');
+    }
+
+    
+
+    const fetchData = (selectedFilter) => {
+        let endpoint = `/${selectedFilter}/?page=${page}&search=${keyword}`;
+        
+        if (selectedFilter === 'pets') {
+            endpoint = `/pets/?search=${keyword}`;
+        } else if (selectedFilter === 'offers') {
+            endpoint = `/offers/?search=${keyword}`;
+        } else if (selectedFilter === 'posts') {
+            endpoint = `/posts/?search=${keyword}`;
+        } else if (selectedFilter === 'users') {
+            endpoint = `/accounts/users/list/?search=${keyword}`;
+        }
+    
+        axiosInstance.get(endpoint)
+            .then((res) => {
+                setResultList(res.data.results);
+                setmaxpages(res.data.total_pages);
+                console.log(res)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     return <>
         <div className="container py-3 h-100">
@@ -35,59 +81,63 @@ const Search = () => {
                 <div className="col-md-3">
                     <div className="d-flex flex-column flex-shrink-0 p-3 bg-light " style={{width:"280px",minHeight:"75vh"}}>
                         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                        
                         <span className="fs-4">Filters</span>
                         </a>
                         <hr/>
                         <ul className="nav nav-pills flex-column mb-auto">
-                        <li>
-                            <a href="/" className="nav-link link-dark">
-                            Users
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/" className="nav-link link-dark">
-                            Posts
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/" className="nav-link link-dark">
+                            <li>
+                                <input type="text" value={keyword} onChange={handleKeywordChange} />
+                                <button className="nav-link link-dark" onClick={handleUsersClick}>
+                                    Users
+                                </button>
+                            </li>
+                            <li>
+                                <button className="nav-link link-dark" onClick={handlePostsClick}>
+                                    Posts
+                                </button>
+                                
+                            </li>
+                            <li>
+                            <button className="nav-link link-dark" onClick={handlePetsClick}>
                             Pets
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/" className="nav-link link-dark">
-                            Offers
-                            </a>
-                        </li>
+                            </button>
+                            </li>
+                            <li>
+                                <button className="nav-link link-dark" onClick={handleOffersClick}>
+                                    Offers
+                                </button>
+                            </li>
                         </ul>
                         </div>
                     </div>
 
 
                 <div className="col-md-9">
+                
+
                     <h2><i className="fa fa-file-o"></i> Result</h2>
                     <hr/>
-                    <p>Showing all results matching "web development"</p>
+                    <p>Showing all results matching "{keyword}"</p>
                     
                     <div className="padding"></div>
                     
                     
                     <div className="table-responsive">
                     <table className="table table-hover">
-                        <tbody><tr>
-                        <td className="number text-center">1</td>
-                        <td className="image"><img src="https://www.bootdey.com/image/400x300/FF8C00" alt=""/></td>
-                        <td className="product"><strong>Product 1</strong><br/>This is the product description.</td>
-                        <td className="rate text-right"><span><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star-half-o"></i></span></td>
-                        <td className="price text-right">$350</td>
+                    <tbody>
+                    {resultList.map((result, index) => (
+                        <tr key={index}>
+                            <td className="number text-center">{index + 1}</td>
+                            
+                            <td className="product"><strong>{result.name}</strong><br/></td>
+                            <td className="product"><strong>{result.content}</strong><br/></td>
+                            <td className="product"><strong>{result.description}</strong><br/></td>
+                            <td className="product"><strong>{result.username}</strong><br/></td>
+                            
+                        
                         </tr>
-                        <tr>
-                        <td className="number text-center">2</td>
-                        <td className="image"><img src="https://www.bootdey.com/image/400x300/5F9EA0" alt=""/></td>
-                        <td className="product"><strong>Product 2</strong><br/>This is the product description.</td>
-                        <td className="rate text-right"><span><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star-o"></i><i className="fa fa-star-o"></i></span></td>
-                        <td className="price text-right">$1,050</td>
-                        </tr>
+                        ))}
                     </tbody></table>
                     </div>
                     
@@ -103,7 +153,7 @@ const Search = () => {
             </div>
             </div>
         </div>
-       </>
+    </>
 };
- 
+
 export default Search;

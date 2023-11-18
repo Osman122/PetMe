641afec6d08d8ec221fdac1c1659dc1aa610f326
Osman122 <osoconan1@gmail.com>
2023-11-18 19:59:52 +0200

    edit profile

---
+++ pet-me/src/components/Profile/profile-pet-card.jsx
@@ -0,0 +1,24 @@
+import {Card} from 'react-bootstrap';
+import '../explore/CardStyles.css'
+import { Link } from 'react-router-dom';
+
+export default function ProfilePetCard (props) {
+    const {id, name, age, thumbnail} = props.pet
+    return <>
+    
+    <Card className='pet-card'>
+        {id? <><Link to={`/petinfo/${id}`}>
+            <Card.Img variant="top" src={thumbnail  || require('../../assets/images/Cat_annon.png')} />
+            </Link></>:
+            <Card.Img variant="top" src={thumbnail  || require('../../assets/images/Cat_annon.png')} 
+        />}
+        
+        <Card.Body className='p-0'>
+            <Card.Text className='text-muted float-end my-2'>{age} Old</Card.Text>
+            <Card.Title className='my-2'><b>{name}</b></Card.Title>
+            {/* <Card.Text className='p-0'>{owner.username}</Card.Text> */}
+        </Card.Body>
+    </Card>
+    </>
+
+}
